05921e91ae2fc19466847a08d2a9860e35b5b957
Can Bartu Kılıç <91592857+cbartuk@users.noreply.github.com>
2023-01-27 17:26:07 +0300

    add and continue

--- components/Post.js
+++ components/Post.js
@@ -12,24 +12,48 @@ import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 
 function Post({ id, username, userImg, img, caption }) {
   return (
-    // 2:06:25
-    <div>
+    <div className="bg-white my-7 border rounded-sm">
       {/* Header */}
-      <div>
-        <img src={userImg} alt="" />
-        <p>{username}</p>
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          alt=""
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+        />
+        <p className="flex-1 font-bold">{username}</p>
         <DotsHorizontalIcon className="h-5" />
       </div>
 
       {/* Img */}
 
+      <img src={img} className="object-cover w-full" alt=""></img>
+
       {/* Buttons */}
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+          <PaperAirplaneIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
 
       {/* Caption */}
+      <p className="p-5 truncate">
+        <span className="font-bold mr-1">{username} </span>
+        {caption}
+      </p>
 
       {/* Comments */}
 
       {/* Input Box */}
+
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input type="text" placeholder="Add a comment..." className="border-none flex-1 focus:ring-0 outline-none" />
+        <button className="font-semibold text-blue-400">Post</button>
+      </form>
+      {/* 2:17:09 https://www.youtube.com/watch?v=a6Xs2Ir40OI */}
     </div>
   );
 }
--- components/Posts.js
+++ components/Posts.js
@@ -1,19 +1,41 @@
 import React from "react";
 import Post from "./Post";
 
+
 const posts = [
   {
     id: "1,2,3",
-    username: "cbartuk",
-    userImg: "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
-    img: "https://images.unsplash.com/photo-1615996330003-8b5c1b0c1f1c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    username: "alexSuprnova",
+    userImg: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
+    img: "https://images.unsplash.com/photo-1668902610778-96403019a7f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     caption: "HEY! This is my first post",
   },
   {
     id: "1,2,3",
-    username: "busefpur",
-    userImg: "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
-    img: "https://images.unsplash.com/photo-1615996330003-8b5c1b0c1f1c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    username: "michealie",
+    userImg: "https://images.unsplash.com/photo-1533689476487-034f57831a58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+    img: "https://images.unsplash.com/photo-1669069509809-5b78376afa69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    caption: "HEY! This is my first post",
+  },
+  {
+    id: "1,2,3",
+    username: "JohnDoe",
+    userImg: "https://images.unsplash.com/photo-1584999734482-0361aecad844?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
+    img: "https://images.unsplash.com/photo-1669039569878-24094915068c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    caption: "HEY! This is my first post",
+  },
+  {
+    id: "1,2,3",
+    username: "JaneDoe",
+    userImg: "https://images.unsplash.com/photo-1518708909080-704599b19972?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    img: "https://images.unsplash.com/photo-1669059921524-327a4c52cff3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+    caption: "HEY! This is my first post",
+  },
+  {
+    id: "1,2,3",
+    username: "alexisBrown",
+    userImg: "https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    img: "https://images.unsplash.com/photo-1669016585654-55d81d7cb8b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
     caption: "HEY! This is my first post",
   },
 ];
