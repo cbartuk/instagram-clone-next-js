c1fd6fa12e38ba4ba97928df7f99b3ac760c5bfe
Can Bartu Kılıç <91592857+cbartuk@users.noreply.github.com>
2023-03-03 21:34:29 +0300

    add the full filled heart icon and functional

--- components/Post.js
+++ components/Post.js
@@ -123,6 +123,12 @@ function Post({ id, username, userImg, img, caption }) {
 
       {/* Caption */}
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1">
+            {likes.length} {likes.length === 1 ? "like" : "likes"}
+          </p>
+        )}
+
         <span className="font-bold mr-1">{username} </span>
         {caption}
       </p>
